150b8b28bea54bdd3efb5e2d121d2dbeb6e2d2cb
rool78 <raul.mazaa@gmail.com>
2022-01-30 16:18:11 +0100

    Add Spring boot depencendy and create first controller

---
+++ applications/main/com/example/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(template, name);
+    }
+}
